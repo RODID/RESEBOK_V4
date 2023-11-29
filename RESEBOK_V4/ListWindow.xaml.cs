7d2a185fcca2fa638199e66dafa9740f9436fb48
RODID <143598047+RODID@users.noreply.github.com>
2023-11-29 10:25:46 +0100

    uppdate

--- RESEBOK_V4/ListWindow.xaml.cs
+++ RESEBOK_V4/ListWindow.xaml.cs
@@ -19,9 +19,40 @@ namespace RESEBOK_V4
     /// </summary>
     public partial class ListWindow : Window
     {
+
+        public List<Destinations> listOfDestinations = new List<Destinations>();
         public ListWindow()
         {
             InitializeComponent();
+            InitializeDestinations();
+        }
+
+        public ListWindow(string cityName, int year, int month, int day)
+        {
+            string CityName = cityName;
+            int Year = year;
+            int Month = month;
+            int Day = day;
+            string Notes = notes;
+        }
+
+        public string cityName { get; set; }
+        public int year { get; set; }   
+        public int month { get; set; }
+        public int day { get; set; }
+        public string notes { get; set; }
+        public void InitializeDestinations()
+        {
+            ListDestination.Items.Add("Spain" + 1975 + 07 + 17);           
+            ListDestination.Items.Add("Stockholm" + 1981 + 12 + 29);
+            ListDestination.Items.Add("" + 1975 + 07 + 17);
+
+
+        }
+
+        private void AddDestination_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
