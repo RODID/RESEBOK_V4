063cb68682500b5a910a76ae29d6e919e2700735
RODID <143598047+RODID@users.noreply.github.com>
2023-11-30 19:28:40 +0100

    update med streamwriter

--- RESEBOK_V4/CsvFileManager.cs
+++ RESEBOK_V4/CsvFileManager.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using RESEBOK_V4;
+using System.IO;
+using System.Threading.Channels;
 
 namespace RESEBOK_V4
 {
@@ -18,7 +20,18 @@ namespace RESEBOK_V4
 
         public void SaveDestinations(List<Destination> destinations)
         {
+            string text = "This is a random text";
 
+            //skapar en ny objekt för att skriva.
+            StreamWriter writer = new StreamWriter("DestinationCSV");
+
+            //skriver en linje
+            writer.WriteLine(text);
+
+            Console.WriteLine("the File was Created!");
+
+            //stänger funktionen
+            writer.Close();
         }
 
     }
