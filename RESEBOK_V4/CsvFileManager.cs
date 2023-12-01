7e92ef326dd1531491aad9bdad94a3519b3cc677
RODID <143598047+RODID@users.noreply.github.com>
2023-12-01 12:59:00 +0100

    update

--- RESEBOK_V4/CsvFileManager.cs
+++ RESEBOK_V4/CsvFileManager.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using RESEBOK_V4;
 using System.IO;
 using System.Threading.Channels;
+using System.Globalization;
+using CsvHelper;
 
 namespace RESEBOK_V4
 {
@@ -20,27 +22,20 @@ namespace RESEBOK_V4
         filePath variable, consider using it in the StreamWriter constructor.*/
     public class CsvFileManager
     {
-        private string filePath;
+        private readonly string filePath;
 
-        public CsvFileManager(string filePath)
+        public CsvFileManager(string Destinations)
         {
-            this.filePath = filePath;
+            this.filePath = Path.Combine(Directory.GetCurrentDirectory(), Destinations);
         }
 
         public void SaveDestinations(List<Destination> destinations)
         {
-            string text = "This is a random text";
-            
-            //skapar en ny objekt för att skriva.
-            StreamWriter writer = new StreamWriter("DestinationCSV");
-
-            //skriver en linje
-            writer.WriteLine(text);
-
-            Console.WriteLine("the File was Created!");
-
-            //stänger funktionen
-            writer.Close();
+            using (StreamWriter writer = new StreamWriter(filePath))
+            using (CsvWriter csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(destinations);
+            }
         }
 
     }
